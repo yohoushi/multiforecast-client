b8a0fba8b517960a5fe18edba9730ab59c0b6693
sonots <sonots@gmail.com>
2013-06-13 17:13:23 +0900

    refactoring

--- lib/multiforecast/shared_examples.rb
+++ lib/multiforecast/shared_examples.rb
@@ -1,14 +1,15 @@
-shared_examples_for 'graph_uri_unit_is_c' do
+shared_examples_for 'graph_uri_long_metrics' do
   it { subject.should match(/(\?|&)t=c(&|$)/) }
 end
 
-shared_examples_for 'graph_uri_unit_is_sc' do
+shared_examples_for 'graph_uri_short_metrics' do
   it { subject.should match(/(\?|&)t=sc(&|$)/) }
 end
 
-shared_examples_for 'graph_uri_custom_params' do
-  it { subject.should match(/(\?|&)from=#{Regexp.escape(URI.escape(from.to_s))}(&|$)/) }
-  it { subject.should match(/(\?|&)to=#{Regexp.escape(URI.escape(to.to_s))}(&|$)/) }
-  it { subject.should match(/(\?|&)width=#{width}(&|$)/) }
-  it { subject.should match(/(\?|&)height=#{height}(&|$)/) }
+shared_examples_for 'graph_uri_params' do
+  it { subject.should match(/(\?|&)t=#{params['t']}(&|$)/) }
+  it { subject.should match(/(\?|&)from=#{Regexp.escape(URI.escape(params['from']))}(&|$)/) }
+  it { subject.should match(/(\?|&)to=#{Regexp.escape(URI.escape(params['to']))}(&|$)/) }
+  it { subject.should match(/(\?|&)width=#{params['width']}(&|$)/) }
+  it { subject.should match(/(\?|&)height=#{params['height']}(&|$)/) }
 end
